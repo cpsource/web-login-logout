b6ce2067df54c4291e6b750d154514a73ba1f378
Cal Page <page.cal@gmail.com>
2024-08-04 06:35:52 -0400

    add tests for PHPMailer

---
+++ PHPMailer/simple-send.php
@@ -0,0 +1,14 @@
+<?php
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->setFrom('admin@natdem.org', 'Your Name');
+$mail->addAddress('page.cal@gmail.com', 'My Friend');
+$mail->Subject  = 'First PHPMailer Message';
+$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
+?>
