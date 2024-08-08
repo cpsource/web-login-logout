d85f0049cb53ae52208187916aec63b63c47dc4c
Cal Page <page.cal@gmail.com>
2024-08-08 19:20:15 +0000

    create a footer

---
+++ natdem/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .custom-hr {
+            border: none;
+            border-top: 2px solid black;
+            width: 50%;
+            margin: 20px auto; /* Center the hr and add padding */
+        }
+        .footer-text {
+            text-align: center;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <footer>
+        <hr class="custom-hr">
+        <div class="footer-text">
+            <p>Copyright &copy; 2002 to 2024, William R. Page, Author</p>
+            <a href="index.php" class="btn btn-primary">Home</a>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
