dc74bfae90a2c962f27222fdada0d3d47a93c039
Cal Page <page.cal@gmail.com>
2024-08-08 14:42:59 -0400

    make a header for natdem

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="path/to/your/image.jpg" alt="Natural Democracy" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <h1 class="text-black">Natural Democracy</h1>
+                <p>Natural Democracy by William R. Page</p>
+            </div>
+            <div class="col-md-4">
+                <!-- This column is empty for now -->
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
