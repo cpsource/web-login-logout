6af435364b263abbc7f6f6cf32b8c000a17c97cc
Cal Page <page.cal@gmail.com>
2024-08-09 09:43:14 +0000

    cleanup

--- natdem/sidebar_updated.php
+++ natdem/sidebar_updated.php
@@ -2,16 +2,16 @@
 
 <!-- GET INFORMED -->
 	<img src="images/pixel.gif" width="5" height="10" alt="" border="0"><br>
-	<table width="110" border="0" cellspacing="0" cellpadding="0">
+	<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
 	<tbody>
 	<tr>
-		<td class="sidebar" colspan="2"><b>INFO</b></td>
+		<td class="sidebar" colspan="2"><b>INFO</b><hr class="light-hr"></td>
 	</tr>
 	<tr>
-		<td class="sidebar"><a href="/?page=author">The Author</a></td>
+		<td class="sidebar"><a href="/?page=author">The Author</a><hr class="light-hr"></td>
 	</tr>
 	<tr>
-		<td class="sidebar"><a href="/?page=book">The Book</a></td>
+		<td class="sidebar"><a href="/?page=book">The Book</a><hr class="light-hr"></td>
 	</tr>
 	<tr>
 		<td class="sidebar"><a
@@ -20,43 +20,43 @@
 echo "href='$cp_url/?page=pressroom'"
 ?>
 
->Press Room</a></td>
+>Press Room</a><hr class="light-hr"></td>
 		</tr>
 		</tbody></table>
 
 <!-- RESOURCES -->
 		<img src="images/pixel.gif" width="1" height="10" alt="" border="0"><br>
-		<table width="110" border="0" cellspacing="0" cellpadding="0">
+		<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
 		<tbody><tr>
-			<td class="sidebar" colspan="2"><b>RESOURCES</b></td>
+			<td class="sidebar" colspan="2"><b>RESOURCES</b><hr class="light-hr"></td>
 		</tr>
 <!--		<tr>
-			<td class="sidebar"><a href="mailto:wrpage@natdem.net">Contact Author</a></td>
+			<td class="sidebar"><a href="mailto:wrpage@natdem.net">Contact Author</a><hr class="light-hr"></td>
 		</tr>
 -->
 		<tr>
-			<td class="sidebar"><a href="/?page=udohr">Human Rights</a></td>
+			<td class="sidebar"><a href="/?page=udohr">Human Rights</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=greatgoals">Great Goals</a></td>
+			<td class="sidebar"><a href="/?page=greatgoals">Great Goals</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=fullpotential">Humanity Reaching Full Potential</a></td>
+			<td class="sidebar"><a href="/?page=fullpotential">Humanity Reaching Full Potential</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=family-pictures">Family Pictures</a></td>
+			<td class="sidebar"><a href="/?page=family-pictures">Family Pictures</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=obituary">Obituary</a></td>
+			<td class="sidebar"><a href="/?page=obituary">Obituary</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=bombs">Atomc Bombs</a></td>
+			<td class="sidebar"><a href="/?page=bombs">Atomc Bombs</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=secretariat">UN Secretariat</a></td>
+			<td class="sidebar"><a href="/?page=secretariat">UN Secretariat</a><hr class="light-hr"></td>
 		</tr>
 		<tr>
-			<td class="sidebar"><a href="/?page=end9">Music and Parting Words</a></td>
+			<td class="sidebar"><a href="/?page=end9">Music and Parting Words</a><hr class="light-hr"></td>
 		</tr>
 
 		</tbody></table>
