116c768edad27f3637ed1a8f984ddd7683572472
Cal Page <page.cal@gmail.com>
2024-08-09 05:46:37 -0400

    formatting changes

--- natdem/sidebar_updated.php
+++ natdem/sidebar_updated.php
@@ -1,64 +1,58 @@
 <!-- Begin sidebar.php -->
-
 <!-- GET INFORMED -->
-	<img src="images/pixel.gif" width="5" height="10" alt="" border="0"><br>
-	<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
-	<tbody>
-	<tr>
-		<td class="sidebar" colspan="2"><b>INFO</b><hr class="light-hr"></td>
-	</tr>
-	<tr>
-		<td class="sidebar"><a href="/?page=author">The Author</a><hr class="light-hr"></td>
-	</tr>
-	<tr>
-		<td class="sidebar"><a href="/?page=book">The Book</a><hr class="light-hr"></td>
-	</tr>
-	<tr>
-		<td class="sidebar"><a
-
+<img src="images/pixel.gif" width="5" height="10" alt="" border="0"><br>
+<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
+<tbody>
+<tr>
+<td class="sidebar" colspan="2"><b>INFO</b><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=author">The Author</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=book">The Book</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a
 <?php
 echo "href='$cp_url/?page=pressroom'"
 ?>
-
 >Press Room</a><hr class="light-hr"></td>
-		</tr>
-		</tbody></table>
-
+</tr>
+</tbody></table>
 <!-- RESOURCES -->
-		<img src="images/pixel.gif" width="1" height="10" alt="" border="0"><br>
-		<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
-		<tbody><tr>
-			<td class="sidebar" colspan="2"><b>RESOURCES</b><hr class="light-hr"></td>
-		</tr>
+<img src="images/pixel.gif" width="1" height="10" alt="" border="0"><br>
+<table width="110" border="0" cellspacing="0" cellpadding="0" class="table table-borderless">
+<tbody><tr>
+<td class="sidebar" colspan="2"><b>RESOURCES</b><hr class="light-hr"></td>
+</tr>
 <!--		<tr>
-			<td class="sidebar"><a href="mailto:wrpage@natdem.net">Contact Author</a><hr class="light-hr"></td>
-		</tr>
+<td class="sidebar"><a href="mailto:wrpage@natdem.net">Contact Author</a><hr class="light-hr"></td>
+</tr>
 -->
-		<tr>
-			<td class="sidebar"><a href="/?page=udohr">Human Rights</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=greatgoals">Great Goals</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=fullpotential">Humanity Reaching Full Potential</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=family-pictures">Family Pictures</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=obituary">Obituary</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=bombs">Atomc Bombs</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=secretariat">UN Secretariat</a><hr class="light-hr"></td>
-		</tr>
-		<tr>
-			<td class="sidebar"><a href="/?page=end9">Music and Parting Words</a><hr class="light-hr"></td>
-		</tr>
-
-		</tbody></table>
-
+<tr>
+<td class="sidebar"><a href="/?page=udohr">Human Rights</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=greatgoals">Great Goals</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=fullpotential">Humanity Reaching Full Potential</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=family-pictures">Family Pictures</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=obituary">Obituary</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=bombs">Atomc Bombs</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=secretariat">UN Secretariat</a><hr class="light-hr"></td>
+</tr>
+<tr>
+<td class="sidebar"><a href="/?page=end9">Music and Parting Words</a><hr class="light-hr"></td>
+</tr>
+</tbody></table>
 <!-- End sidebar.php -->
