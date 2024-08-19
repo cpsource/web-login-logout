17a5f4de4ce86dfd1adfe5048a09768a7d91e7e2
Cal Page <page.cal@gmail.com>
2024-08-19 11:26:54 +0000

    repoint to new repository

--- check-apache2/point-web-to-natdem.sh
+++ check-apache2/point-web-to-natdem.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Check if the symlink /var/www/html points to html-natdem-new
-if [ "$(ls -l /var/www/html | awk '{print $NF}')" = "html-natdem-new" ]; then
-  # Exit if it already points to html-natdem-new
-  echo "/var/www/html already points to html-natdem-new. Exiting."
-  logger -p local3.notice -t ubuntu.cron "/var/www/html already points to html-natdem-new. Exiting."
+# Check if the symlink /var/www/html points to natdem
+if [ "$(ls -l /var/www/html | awk '{print $NF}')" = "natdem" ]; then
+  # Exit if it already points to natdem
+  echo "/var/www/html already points to natdem. Exiting."
+  logger -p local3.notice -t ubuntu.cron "/var/www/html already points to natdem. Exiting."
   exit 0
 else
   # Change directory to /var/www
@@ -13,12 +13,12 @@ else
   # Remove the current html symlink
   sudo rm -f html || { echo "Failed to remove the current html symlink"; logger -p local3.notice -t ubuntu.cron "Failed to remove the current html symlink"; exit 1; }
 
-  # Create a new symlink pointing to html-natdem-new
-  sudo ln -s html-natdem-new html || { echo "Failed to create a new symlink"; logger -p local3.notice -t ubuntu.cron "Failed to create a new symlink"; exit 1; }
+  # Create a new symlink pointing to natdem
+  sudo ln -s html-natdem html || { echo "Failed to create a new symlink"; logger -p local3.notice -t ubuntu.cron "Failed to create a new symlink"; exit 1; }
 
   # Print success message and exit
-  echo "Updated /var/www/html to point to html-natdem-new."
-  logger -p local3.notice -t ubuntu.cron "Updated /var/www/html to point to html-natdem-new."
+  echo "Updated /var/www/html to point to natdem."
+  logger -p local3.notice -t ubuntu.cron "Updated /var/www/html to point to natdem."
   exit 0
 fi
 
