60423eb4a69a4ff5d9080a4d993b37fa5773cc78
Cal Page <page.cal@gmail.com>
2024-08-24 10:22:06 +0000

    add comments

--- see-certificate.sh
+++ see-certificate.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo "SSL Certificate information is kept here:";
+echo "  /etc/ssl/ca-bundlke.crt";
+echo "  /etc/ssl/certificate.crt";
+echo "  /etc/ssl/private/private.key";
+echo ""
+echo "Now displaying certificate.crt:"
+echo ""
 openssl x509 -in /etc/ssl/certificate.crt -text -noout
