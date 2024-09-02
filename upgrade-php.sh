9d3e8153decf3e278d097790e63738c390dab027
Cal Page <page.cal@gmail.com>
2024-09-02 04:37:34 +0000

    upgrade php to 8.2

---
+++ upgrade-php.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/apache2
+sudo apt install php8.2
+sudo apt update
+
